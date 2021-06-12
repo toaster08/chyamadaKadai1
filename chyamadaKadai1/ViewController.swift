69556aa679a8fb6f8df9a26b93ab756dfdcdbccd
toaster <>
2021-06-12 17:36:38 +0900

    KeyboardがTextFieldにかかって見えなくなる問題に対して UITapGestureによるendEdittingの実装

--- chyamadaKadai1/ViewController.swift
+++ chyamadaKadai1/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak private var field1: UITextField!
     @IBOutlet weak private var field2: UITextField!
     @IBOutlet weak private var field3: UITextField!
     @IBOutlet weak private var field4: UITextField!
     @IBOutlet weak private var field5: UITextField!
     @IBOutlet weak private var result: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // MARK: 以下.endEditingまではネット上で調べて実装
+        let tapGR: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGR.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tapGR)
+    }
+
+    @objc private func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
 
     @IBAction private func calculate(_ sender: UIButton) {
         let number1 = Int(field1.text!) ?? 0
